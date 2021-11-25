6d6120804d16e263f03a877cc5f4d0c72bb8b3c6
Carlos S <carlos.sedano@oracle.com>
2021-11-24 21:17:53 -0600

    Gradle build was failing due to a test

--- src/test/java/com/game/mancala/integration/mapper/MancalaMapperIntegrationTest.java
+++ src/test/java/com/game/mancala/integration/mapper/MancalaMapperIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.game.mancala.integration.mapper;
 
 import com.game.mancala.domain.dto.requests.MoveRequestDTO;
+import com.game.mancala.domain.dto.requests.PlayerMoveRequestDTO;
 import com.game.mancala.domain.dto.requests.StartRequestDTO;
 import com.game.mancala.domain.dto.responses.BoardResponseDTO;
 import com.game.mancala.domain.dto.responses.EndGameResponseDTO;
@@ -15,11 +16,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static com.game.mancala.domain.dto.responses.PlayerTypeResponse.PLAYER_ONE;
 import static com.game.mancala.domain.dto.responses.PlayerTypeResponse.PLAYER_TWO;
-import static com.game.mancala.utils.MancalaMockData.asJsonString;
+import static com.game.mancala.utils.Constants.EMPTY_PIT;
+import static com.game.mancala.utils.MancalaMockData.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(SpringExtension.class)
@@ -56,20 +59,52 @@ class MancalaMapperIntegrationTest {
 
     @Test
     void moveRequestDTOToMancalaResponseDTO() {
-        final BoardResponseDTO boardResponseDTO = mancalaMockData.boardResponseDTO;
+        final BoardResponseDTO boardResponseDTO = BoardResponseDTO.builder()
+                .bigPit(ZERO_BIG_PIT)
+                .pits(PLAYER_INIT_BOARD)
+                .build();
 
-        final List<PlayerResponseDTO> playerResponseDTOList = mancalaMockData.playerResponseDTOList;
-        playerResponseDTOList.get(PLAYER_ONE.playerTypeValue).setBoard(boardResponseDTO);
-        playerResponseDTOList.get(PLAYER_ONE.playerTypeValue).setPlayerTurn(true);
+        final List<PlayerResponseDTO> playerResponseDTOList = Arrays.asList(
+                PlayerResponseDTO.builder()
+                        .playerType(PLAYER_ONE)
+                        .name(PLAYER_ONE_NAME)
+                        .isPlayerTurn(false)
+                        .board(
+                                BoardResponseDTO.builder()
+                                        .bigPit(EMPTY_PIT).
+                                        pits(PLAYER_INIT_BOARD).build()
+                        ).build(),
+                PlayerResponseDTO.builder()
+                        .playerType(PLAYER_TWO)
+                        .name(PLAYER_TWO_NAME)
+                        .isPlayerTurn(true)
+                        .board(
+                                BoardResponseDTO.builder()
+                                        .bigPit(EMPTY_PIT).
+                                        pits(PLAYER_INIT_BOARD).build()
+                        ).build());
 
-        playerResponseDTOList.get(PLAYER_TWO.playerTypeValue).setBoard(boardResponseDTO);
-        playerResponseDTOList.get(PLAYER_TWO.playerTypeValue).setPlayerTurn(false);
+        final List<PlayerMoveRequestDTO> playerMoveRequestDTOList = Arrays.asList(
+                PlayerMoveRequestDTO.builder()
+                        .playerType(PLAYER_ONE)
+                        .name(PLAYER_ONE_NAME)
+                        .isPlayerTurn(false)
+                        .board(boardResponseDTO).build(),
+                PlayerMoveRequestDTO.builder()
+                        .playerType(PLAYER_TWO)
+                        .name(PLAYER_TWO_NAME)
+                        .isPlayerTurn(true)
+                        .board(boardResponseDTO).build()
+        );
 
-        final MoveRequestDTO moveRequestDTO = mancalaMockData.moveRequestDTO;
+        final MoveRequestDTO moveRequestDTO =MoveRequestDTO.builder()
+                .pit(EMPTY_PIT)
+                .players(playerMoveRequestDTOList)
+                .build();
 
 
         final MancalaResponseDTO expectedMancalaResponseDTO = MancalaResponseDTO.builder()
-                .players(mancalaMockData.playerResponseDTOList)
+                .players(playerResponseDTOList)
                 .gameEnded(false)
                 .build();
 
